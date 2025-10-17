7c93c5564c4c3764ac19187a6efba6998f021415
Daniel Colmenares Salamanca <danicolms@Daniels-MacBook-Air.local>
2025-10-17 21:09:01 +0200

    fix: remove ignition, since we already have deploy-standalone for an easier deployment

--- ignition/modules/ForeverMessage.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const ForeverMessageModule = buildModule("ForeverMessageModule", (m) => {
-  const foreverMessage = m.contract("ForeverMessage");
-
-  return { foreverMessage };
-});
-
-export default ForeverMessageModule;
